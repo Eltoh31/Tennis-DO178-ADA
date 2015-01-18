94f28a8ed5ef06cff2c5ccf3a9a2a254d74bf7d4
Martel Loic <loic.martel31@gmail.com>
2015-01-18 11:27:15 +0100

    stttttttttttttttttlink

--- tennis.adb
+++ tennis.adb
@@ -1,9 +1,9 @@
-with  Screen_Interface; use Screen_Interface;
+with Screen_Interface; use Screen_Interface;
 
 procedure Tennis is 
 begin
    Screen_Interface.Initialize;
-   
+   Screen_Interface.Fill_Screen();
    
 end Tennis;
   
